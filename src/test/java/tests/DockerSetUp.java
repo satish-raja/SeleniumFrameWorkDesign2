bdf0550f4ee6a126f3bb065c1e7f46eddeb9b754
satishraja <satishraja4u@gmail.com>
2024-01-14 13:22:51 +0530

    COMMIT 14-01-2024 13:22

--- src/test/java/tests/DockerSetUp.java
+++ src/test/java/tests/DockerSetUp.java
@@ -7,42 +7,43 @@ import org.testng.annotations.BeforeTest;
 
 public class DockerSetUp {
 
-	@BeforeTest
+    @BeforeTest
     void startDockergrid() throws IOException, InterruptedException {
-        ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c","start", "start_dockergrid.bat");
-        processBuilder.redirectErrorStream(true); // Redirect error stream to output stream
+        ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", "start", "start_dockergrid.bat");
+        processBuilder.redirectErrorStream(true);
 
-        try {
-        	Process startProcess = processBuilder.start();
-             Thread.sleep(150000);// Wait for the process to finish
-             } catch (IOException e) {
+        try  {
+        	Process startProcess = processBuilder.start() ;
+            Thread.sleep(120000);
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
-             }
+        }
+
         System.out.println("Docker SetUp Process Completed");
     }
-		
+
     @AfterTest
     void stopDockergrid() throws IOException, InterruptedException {
-        // Start the stop_dockergrid.bat in a new process
         ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", "start", "stop_dockergrid.bat");
-        processBuilder.redirectErrorStream(true); // Redirect error stream to output stream
+        processBuilder.redirectErrorStream(true);
 
-        try {
-            Process stopProcess = processBuilder.start();
+        try  {
+        	Process stopProcess = processBuilder.start();
             stopProcess.waitFor(); // Wait for the process to finish
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        // Sleep for a short duration if needed
-        Thread.sleep(15000);
-        // Terminate any remaining cmd.exe processes
-        try {
-            Process killProcess = new ProcessBuilder("taskkill", "/f", "/im", "cmd.exe").start();
-            killProcess.waitFor(); // Wait for the process to finish
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Thread.sleep(30000);   
+
+        Thread.sleep(15000); // Sleep for a short duration if needed
+
+//        try {
+//            Process killProcess = new ProcessBuilder("taskkill", "/f", "/im", "cmd.exe").start();
+//            killProcess.waitFor(); // Wait for the process to finish
+//        } catch (IOException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+//        Thread.sleep(30000);
         System.out.println("Docker Process Closed");
     }
 }
